72f96c7648f7867aa7ba1626743e219f53b225ac
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2024-05-30 05:06:06 +0200

    Update

--- chap2_norm.R
+++ chap2_norm.R
@@ -19,26 +19,21 @@ x <- time(y)
 sw_size <- 5
 
 test_size <- 10 
-range_a <- length(y)-75-1
-range_b <- 7
+range <- length(y)-77-1
 
 swy <- ts_data(y, sw_size)
 
-swtrain <- swy[1:(nrow(swy)-test_size)]
-swtrain <- swy#[1:(nrow(swy)-test_size)]
-sw_range_a <- swy[(nrow(swy)-range_a+1),]
-sw_range_b <- swy[(nrow(swy)-range_b+1),]
+sw_range <- swy[(nrow(swy)-range+1),]
+
 
 grf <- autoplot(y) + theme_bw(base_size = 10) + geom_point(size=1)
 grf <- grf + theme(plot.title = element_blank())
 grf <- grf + theme(panel.grid.major = element_blank()) + theme(panel.grid.minor = element_blank())
 grf <- grf + ylab("temperature")
 grf <- grf + xlab("year")
-grf <- grf + geom_vline(xintercept = x[length(x)-sw_size+1-range_a+1], col="darkgreen", linewidth = 0.5, linetype="dotted")
-grf <- grf + geom_vline(xintercept = x[length(x)-range_a+1], col="darkgreen", linewidth = 0.5, linetype="dotted")
-grf <- grf + geom_vline(xintercept = x[length(x)-sw_size+1-range_b+1], col="red", linewidth = 0.5, linetype="dotted")
-grf <- grf + geom_vline(xintercept = x[length(x)-range_b+1], col="red", linewidth = 0.5, linetype="dotted")
-grf <- grf + labs(caption = "Intervals A and B") 
+grf <- grf + geom_vline(xintercept = x[length(x)-sw_size+1-range+1], col="darkgreen", linewidth = 0.5, linetype="dotted")
+grf <- grf + geom_vline(xintercept = x[length(x)-range+1], col="darkgreen", linewidth = 0.5, linetype="dotted")
+grf <- grf + labs(caption = "(a)") 
 grf <- grf + theme(plot.caption = element_text(hjust = 0.5))
 grf <- grf  + font
 grfS <- grf
@@ -92,51 +87,37 @@ gen_graphics <- function(dgminmax, ddiff, dswminmax, d_an) {
 }
 
 ngminmax <- ts_norm_gminmax(remove_outliers = FALSE)
-ngminmax <- fit(ngminmax, swtrain)
-dgminmax <- transform(ngminmax, sw_range_a)
-dgminmaxt <- transform(ngminmax, sw_range_b)
+ngminmax <- fit(ngminmax, swy)
+dgminmax <- transform(ngminmax, sw_range)
 
 ### ts_diff
 
 ndiff <- ts_norm_diff(remove_outliers = FALSE)
-ndiff <- fit(ndiff, swtrain)
-ddiff <- transform(ndiff, sw_range_a)
-ddifft <- transform(ndiff, sw_range_b)
+ndiff <- fit(ndiff, swy)
+ddiff <- transform(ndiff, sw_range)
 
 ### ts_swminmax()
 
 nswminmax <- ts_norm_swminmax(remove_outliers = FALSE)
-nswminmax <- fit(nswminmax, swtrain)
-dswminmax <- transform(nswminmax, sw_range_a)
-dswminmaxt <- transform(nswminmax, sw_range_b)
+nswminmax <- fit(nswminmax, swy)
+dswminmax <- transform(nswminmax, sw_range)
 
 ### ts_an()
 
 n_an <- ts_norm_an()
-n_an <- fit(n_an, swtrain)
-d_an <- transform(n_an, sw_range_a)
-d_ant <- transform(n_an, sw_range_b)
-
-
-dgminmaxt[dgminmaxt>1] <- 1
+n_an <- fit(n_an, swy)
+d_an <- transform(n_an, sw_range)
 
 list <- gen_graphics(dgminmax, ddiff, dswminmax, d_an)
-list$grfGA <- list$grfGA + labs(caption = "(A)") + theme(plot.caption = element_text(colour = "darkgreen"))
-list$grfDA <- list$grfDA + labs(caption = "(A)") + theme(plot.caption = element_text(colour = "darkgreen"))
-list$grfMA <- list$grfMA + labs(caption = "(A)") + theme(plot.caption = element_text(colour = "darkgreen"))
-list$grfAA <- list$grfAA + labs(caption = "(A)") + theme(plot.caption = element_text(colour = "darkgreen"))
-
-list_t <- gen_graphics(dgminmaxt, ddifft, dswminmaxt, d_ant)
-list_t$grfGA <- list_t$grfGA + labs(caption = "(B)") + theme(plot.caption = element_text(colour = "red"))
-list_t$grfDA <- list_t$grfDA + labs(caption = "(B)") + theme(plot.caption = element_text(colour = "red"))
-list_t$grfMA <- list_t$grfMA + labs(caption = "(B)") + theme(plot.caption = element_text(colour = "red"))
-list_t$grfAA <- list_t$grfAA + labs(caption = "(B)") + theme(plot.caption = element_text(colour = "red"))
+list$grfGA <- list$grfGA + labs(caption = "(b)") + theme(plot.caption = element_text(colour = "darkgreen"))
+list$grfDA <- list$grfDA + labs(caption = "(c)") + theme(plot.caption = element_text(colour = "darkgreen"))
+list$grfMA <- list$grfMA + labs(caption = "(d)") + theme(plot.caption = element_text(colour = "darkgreen"))
+list$grfAA <- list$grfAA + labs(caption = "(e)") + theme(plot.caption = element_text(colour = "darkgreen"))
 
 
-mypng(file="figures/chap2_normb.png", width=1920, height=1080) 
-gridExtra::grid.arrange(grfS, list$grfGA, list$grfDA, list$grfMA, list$grfAA, 
-                        list_t$grfGA, list_t$grfDA, list_t$grfMA, list_t$grfAA,
-                        layout_matrix = matrix(c(1,1,1,1,2,3,4,5,6,7,8,9), byrow = TRUE, ncol = 4))
+mypng(file="figures/chap2_norm.png", width=1920, height=1080) 
+gridExtra::grid.arrange(grfS, list$grfGA, list$grfDA, list$grfMA, list$grfAA,
+                        layout_matrix = matrix(c(1,1,1,1,2,3,4,5), byrow = TRUE, ncol = 4))
 dev.off() 
 
 
